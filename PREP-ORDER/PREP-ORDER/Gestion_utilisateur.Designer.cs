8e49f8a50918aeea9b2f8463ba50c870fd4e452e
LangBapt <baptistelanglois0@gmail.com>
2024-12-06 17:23:55 +0100

    <feat><création formulaire gestion>
    <création du formulaire qui va servir à la gestion des utilisateurs>

---
+++ PREP-ORDER/PREP-ORDER/Gestion_utilisateur.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PREP_ORDER
+{
+    partial class Gestion_utilisateur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lvUser = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // lvUser
+            // 
+            lvUser.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            lvUser.Location = new Point(12, 12);
+            lvUser.Name = "lvUser";
+            lvUser.Size = new Size(322, 426);
+            lvUser.TabIndex = 0;
+            lvUser.UseCompatibleStateImageBehavior = false;
+            lvUser.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Login";
+            columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Zone";
+            // 
+            // Gestion_utilisateur
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lvUser);
+            Name = "Gestion_utilisateur";
+            Text = "Gestion_utilisateur";
+            Load += Gestion_utilisateur_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView lvUser;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+    }
+}
---
+++ PREP-ORDER/PREP-ORDER/Gestion_utilisateur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic.Logging;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace PREP_ORDER
+{
+    public partial class Gestion_utilisateur : Form
+    {
+        public Gestion_utilisateur()
+        {
+            InitializeComponent();
+        }
+
+        private void Gestion_utilisateur_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
