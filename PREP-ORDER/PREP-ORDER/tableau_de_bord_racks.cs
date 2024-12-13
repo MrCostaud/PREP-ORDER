ef322d49b012d7174c683008011751bfdc8f199b
MrCostaud <simarikalban@gmail.com>
2024-12-13 10:59:14 +0100

    <features> tableau de bord : ajouté le squelette de la fenêtre

---
+++ PREP-ORDER/PREP-ORDER/tableau_de_bord_racks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PREP_ORDER
+{
+    public partial class tableau_de_bord_racks : Form
+    {
+        public tableau_de_bord_racks()
+        {
+            InitializeComponent();
+        }
+    }
+}
