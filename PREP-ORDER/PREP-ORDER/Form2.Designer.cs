ea881b69793f6c7dffda7c11b7844371be674695
Theo Laurent <theo.laurent-potel@stadjutor.com>
2024-12-13 17:25:43 +0100

    tableau de bord terminé

--- PREP-ORDER/PREP-ORDER/Form2.Designer.cs
+++ PREP-ORDER/PREP-ORDER/Form2.Designer.cs
@@ -33,20 +33,42 @@
             comboBox1 = new ComboBox();
             pictureBox1 = new PictureBox();
             label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(224, 120);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(1089, 400);
+            dataGridView1.Size = new Size(1271, 400);
             dataGridView1.TabIndex = 0;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // label1
             // 
@@ -87,11 +109,231 @@
             label2.TabIndex = 4;
             label2.Text = "Trier par zone de préparation :";
             // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.Location = new Point(1518, 132);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 35);
+            button1.TabIndex = 5;
+            button1.Text = "VALIDER";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(0, 192, 0);
+            button2.Location = new Point(1518, 167);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 35);
+            button2.TabIndex = 6;
+            button2.Text = "VALIDER";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(0, 192, 0);
+            button3.Location = new Point(1518, 202);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 35);
+            button3.TabIndex = 7;
+            button3.Text = "VALIDER";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(0, 192, 0);
+            button4.Location = new Point(1518, 237);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 35);
+            button4.TabIndex = 8;
+            button4.Text = "VALIDER";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(0, 192, 0);
+            button5.Location = new Point(1518, 272);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 35);
+            button5.TabIndex = 9;
+            button5.Text = "VALIDER";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(0, 192, 0);
+            button6.Location = new Point(1518, 307);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 35);
+            button6.TabIndex = 10;
+            button6.Text = "VALIDER";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(0, 192, 0);
+            button7.Location = new Point(1518, 342);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 35);
+            button7.TabIndex = 11;
+            button7.Text = "VALIDER";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(0, 192, 0);
+            button8.Location = new Point(1518, 377);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 35);
+            button8.TabIndex = 12;
+            button8.Text = "VALIDER";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(0, 192, 0);
+            button9.Location = new Point(1518, 412);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 35);
+            button9.TabIndex = 13;
+            button9.Text = "VALIDER";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(0, 192, 0);
+            button10.Location = new Point(1518, 447);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 35);
+            button10.TabIndex = 14;
+            button10.Text = "VALIDER";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Red;
+            button11.Location = new Point(1627, 132);
+            button11.Name = "button11";
+            button11.Size = new Size(94, 35);
+            button11.TabIndex = 15;
+            button11.Text = "Zone vide";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Red;
+            button12.Location = new Point(1627, 167);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 35);
+            button12.TabIndex = 16;
+            button12.Text = "Zone vide";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Red;
+            button13.Location = new Point(1627, 202);
+            button13.Name = "button13";
+            button13.Size = new Size(94, 35);
+            button13.TabIndex = 17;
+            button13.Text = "Zone vide";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.Red;
+            button14.Location = new Point(1627, 237);
+            button14.Name = "button14";
+            button14.Size = new Size(94, 35);
+            button14.TabIndex = 18;
+            button14.Text = "Zone vide";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Red;
+            button15.Location = new Point(1627, 272);
+            button15.Name = "button15";
+            button15.Size = new Size(94, 35);
+            button15.TabIndex = 19;
+            button15.Text = "Zone vide";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Red;
+            button16.Location = new Point(1627, 307);
+            button16.Name = "button16";
+            button16.Size = new Size(94, 35);
+            button16.TabIndex = 20;
+            button16.Text = "Zone vide";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.Red;
+            button17.Location = new Point(1627, 342);
+            button17.Name = "button17";
+            button17.Size = new Size(94, 35);
+            button17.TabIndex = 21;
+            button17.Text = "Zone vide";
+            button17.UseVisualStyleBackColor = false;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.Red;
+            button18.Location = new Point(1627, 377);
+            button18.Name = "button18";
+            button18.Size = new Size(94, 35);
+            button18.TabIndex = 22;
+            button18.Text = "Zone vide";
+            button18.UseVisualStyleBackColor = false;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.Red;
+            button19.Location = new Point(1627, 412);
+            button19.Name = "button19";
+            button19.Size = new Size(94, 35);
+            button19.TabIndex = 23;
+            button19.Text = "Zone vide";
+            button19.UseVisualStyleBackColor = false;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.Red;
+            button20.Location = new Point(1627, 447);
+            button20.Name = "button20";
+            button20.Size = new Size(94, 35);
+            button20.TabIndex = 24;
+            button20.Text = "Zone vide";
+            button20.UseVisualStyleBackColor = false;
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1472, 575);
+            ClientSize = new Size(1744, 611);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
             Controls.Add(comboBox1);
@@ -112,5 +354,25 @@
         private ComboBox comboBox1;
         private PictureBox pictureBox1;
         private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
     }
 }
--- PREP-ORDER/PREP-ORDER/Form2.cs
+++ PREP-ORDER/PREP-ORDER/Form2.cs
@@ -2,7 +2,9 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using WinFormsButton = System.Windows.Forms.Button;
 namespace PREP_ORDER
 {
     public partial class Form2 : Form
@@ -10,13 +12,13 @@ namespace PREP_ORDER
         public Form2()
         {
             InitializeComponent();
-            this.Load += new EventHandler(Form2_Load); // Associer l'événement Load
+            this.Load += new EventHandler(Form2_Load);
         }
 
         private void ChargerDonneesZone()
         {
-            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder;Trusted_Connection=True;";
-            string query = "SELECT c.numCommande, c.dateCommande, m.numMagasin, m.nomMagasin, z.codeZone,  z.libelleZone, libelleProduit, qtLotProduit FROM  COMMANDE c INNER JOIN MAGASIN m ON c.numMagasin = m.numMagasin INNER JOIN SOUS_COMMANDE ON numSousComm = c.numCommande INNER JOIN PRODUIT p ON numProduit = numSousComm INNER JOIN ZONE z ON z.codeZone = p.codeZone inner join PALETTE on p.numProduit = numPalette;";
+            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder1;Trusted_Connection=True;";
+            string query = " SELECT top 10 c.numCommande, c.dateCommande, m.numMagasin, m.nomMagasin, z.codeZone,  z.libelleZone, p.libelleProduit, qtLotProduit, c.statut, Probleme \r\nFROM  COMMANDE c \r\nINNER JOIN MAGASIN m ON c.numMagasin = m.numMagasin \r\nINNER JOIN ZONE z ON z.codeZone = m.codeZone \r\nINNER JOIN PRODUIT p ON z.codeZone = p.codeZone \r\ninner join PALETTE on p.numProduit = numPalette \r\nwhere statut = 'En cours';";
 
             try
             {
@@ -26,24 +28,115 @@ namespace PREP_ORDER
                     DataTable dataTable = new DataTable();
 
                     adapter.Fill(dataTable);
-                    dataGridView1.DataSource = dataTable; // Vérifiez que le DataGridView s'appelle bien dataGridView1
+                    dataGridView1.DataSource = dataTable;
                 }
+
+
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            GérerAffichageBoutons();
+
         }
+
+        private void GérerAffichageBoutons()
+        {
+
+            int rowCount = dataGridView1.Rows.Count;
+
+
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+
+
+
+            button11.Visible = false;
+            button12.Visible = false;
+            button13.Visible = false;
+            button14.Visible = false;
+            button15.Visible = false;
+            button16.Visible = false;
+            button17.Visible = false;
+            button18.Visible = false;
+            button19.Visible = false;
+            button20.Visible = false;
+
+
+
+            if (rowCount >= 1) button11.Visible = true;
+            if (rowCount >= 2) button12.Visible = true;
+            if (rowCount >= 3) button13.Visible = true;
+            if (rowCount >= 4) button14.Visible = true;
+            if (rowCount >= 5) button15.Visible = true;
+            if (rowCount >= 6) button16.Visible = true;
+            if (rowCount >= 7) button17.Visible = true;
+            if (rowCount >= 8) button18.Visible = true;
+            if (rowCount >= 9) button19.Visible = true;
+            if (rowCount >= 10) button20.Visible = true;
+
+
+
+
+            if (rowCount >= 1) button1.Visible = true;
+            if (rowCount >= 2) button2.Visible = true;
+            if (rowCount >= 3) button3.Visible = true;
+            if (rowCount >= 4) button4.Visible = true;
+            if (rowCount >= 5) button5.Visible = true;
+            if (rowCount >= 6) button6.Visible = true;
+            if (rowCount >= 7) button7.Visible = true;
+            if (rowCount >= 8) button8.Visible = true;
+            if (rowCount >= 9) button9.Visible = true;
+            if (rowCount >= 10) button10.Visible = true;
+
+
+
+
+        }
+
 
         private void Form2_Load(object sender, EventArgs e)
         {
             ChargerDonneesZone();
+
+
+            button1.Click += Bouton_Click;
+            button2.Click += Bouton_Click;
+            button3.Click += Bouton_Click;
+            button4.Click += Bouton_Click;
+            button5.Click += Bouton_Click;
+            button6.Click += Bouton_Click;
+            button7.Click += Bouton_Click;
+            button8.Click += Bouton_Click;
+            button9.Click += Bouton_Click;
+            button10.Click += Bouton_Click;
+            button11.Click += Bouton_Click2;
+            button12.Click += Bouton_Click2;
+            button13.Click += Bouton_Click2;
+            button14.Click += Bouton_Click2;
+            button15.Click += Bouton_Click2;
+            button16.Click += Bouton_Click2;
+            button17.Click += Bouton_Click2;
+            button18.Click += Bouton_Click2;
+            button19.Click += Bouton_Click2;
+            button20.Click += Bouton_Click2;
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder;Trusted_Connection=True;";
-            string query = $"SELECT c.numCommande, c.dateCommande, m.numMagasin, m.nomMagasin, z.codeZone,  z.libelleZone, libelleProduit, qtLotProduit FROM  COMMANDE c INNER JOIN MAGASIN m ON c.numMagasin = m.numMagasin INNER JOIN SOUS_COMMANDE ON numSousComm = c.numCommande INNER JOIN PRODUIT p ON numProduit = numSousComm INNER JOIN ZONE z ON z.codeZone = p.codeZone inner join PALETTE on p.numProduit = numPalette WHERE libelleZone = '{comboBox1.Text}';";
+            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder1;Trusted_Connection=True;";
+            string query = $"SELECT top 10 c.numCommande, c.dateCommande, m.numMagasin, m.nomMagasin, z.codeZone,  z.libelleZone, p.libelleProduit, qtLotProduit, c.statut, Probleme \r\nFROM  COMMANDE c \r\nINNER JOIN MAGASIN m ON c.numMagasin = m.numMagasin \r\nINNER JOIN ZONE z ON z.codeZone = m.codeZone \r\nINNER JOIN PRODUIT p ON z.codeZone = p.codeZone \r\ninner join PALETTE on p.numProduit = numPalette WHERE libelleZone = '{comboBox1.Text}' and statut = 'En cours';";
 
             try
             {
@@ -53,8 +146,137 @@ namespace PREP_ORDER
                     DataTable dataTable = new DataTable();
 
                     adapter.Fill(dataTable);
-                    dataGridView1.DataSource = dataTable; // Vérifiez que le DataGridView s'appelle bien dataGridView1
+                    dataGridView1.DataSource = dataTable;
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            GérerAffichageBoutons();
+        }
+
+
+
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "statut" && dataGridView1.Columns[e.ColumnIndex].Name == "Probleme")
+            {
+
+                int numCommande = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["numCommande"].Value);
+                bool statut = Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells["statut"].Value);
+                bool Probleme = Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells["Probleme"].Value);
+
+
+                string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder1;Trusted_Connection=True;";
+                string query = "UPDATE COMMANDE SET statut = @statut, Probleme = @Probleme WHERE numCommande = @numCommande";
+
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@statut", statut);
+                        command.Parameters.AddWithValue("@numCommande", numCommande);
+                        command.Parameters.AddWithValue("@Probleme", Probleme);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+
+
+                    ChargerDonneesZone();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+
+
+        private void Bouton_Click(object sender, EventArgs e)
+        {
+            // Identifier le bouton cliqué
+            System.Windows.Forms.Button boutonClique = sender as System.Windows.Forms.Button;
+
+            // Trouver l'index de la ligne correspondante
+            int rowIndex = int.Parse(boutonClique.Name.Replace("button", "")) - 1;
+
+            // Vérifier si l'index est valide
+            if (rowIndex < dataGridView1.Rows.Count)
+            {
+                // Récupérer le `numCommande` de la ligne correspondante
+                int numCommande = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["numCommande"].Value);
+
+                // Mettre à jour le statut dans la base de données
+                MettreAJourStatut(numCommande, "Terminé");
+
+                // Rafraîchir l'affichage
+                ChargerDonneesZone();
+            }
+
+        }
+
+
+
+        private void Bouton_Click2(object sender, EventArgs e)
+        {
+            // Identifier le bouton cliqué
+            System.Windows.Forms.Button boutonClique1 = sender as System.Windows.Forms.Button;
+
+            // Trouver l'index de la ligne correspondante (ajuster pour boutons après 10)
+            int rowIndex = int.Parse(boutonClique1.Name.Replace("button", "")) - 11;
+
+            // Vérifier si l'index est valide
+            if (rowIndex >= 0 && rowIndex < dataGridView1.Rows.Count)
+            {
+                // Récupérer le `numCommande` de la ligne correspondante
+                int numCommande = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["numCommande"].Value);
+
+                // Mettre à jour le statut ou le problème dans la base de données
+                MettreAJourStatut1(numCommande, "Erreur");
+
+                // Rafraîchir l'affichage
+                ChargerDonneesZone();
+            }
+            else
+            {
+                MessageBox.Show("Le bouton n'est pas associé à une ligne valide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+
+
+
+
+        private void MettreAJourStatut(int numCommande, string nouveauStatut)
+        {
+            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder1;Trusted_Connection=True;";
+            string query = "UPDATE COMMANDE SET statut = @statut WHERE numCommande = @numCommande";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@statut", nouveauStatut);
+                    command.Parameters.AddWithValue("@numCommande", numCommande);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                MessageBox.Show($"Le statut de la commande {numCommande} a été mis à jour à '{nouveauStatut}'.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
@@ -62,9 +284,39 @@ namespace PREP_ORDER
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
+
+
+
+
+
+
+        private void MettreAJourStatut1(int numCommande, string nouveauProbleme)
+        {
+            string connectionString = "Server=SIO2023-23\\SQLEXPRESS;Database=preporder1;Trusted_Connection=True;";
+            string query = "UPDATE COMMANDE SET Probleme = @Probleme WHERE numCommande = @numCommande";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Probleme", nouveauProbleme);
+                    command.Parameters.AddWithValue("@numCommande", numCommande);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                MessageBox.Show($"La notification Probléme de la commande {numCommande} a été mis à jour à '{nouveauProbleme}'.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
+
     }
 }
