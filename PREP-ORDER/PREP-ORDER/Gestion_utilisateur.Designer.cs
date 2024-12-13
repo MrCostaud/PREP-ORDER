f4d3efc462d9397e5473201c9d7fc0c770020e2c
LangBapt <baptistelanglois0@gmail.com>
2024-12-13 17:11:35 +0100

    <feat><modification utilisateur>
    <interface et fonctionnalité de modification du login et de la zone(sauf responsable) d'un utilisateur déjà existant>

--- PREP-ORDER/PREP-ORDER/Gestion_utilisateur.Designer.cs
+++ PREP-ORDER/PREP-ORDER/Gestion_utilisateur.Designer.cs
@@ -33,6 +33,24 @@
             columnHeader2 = new ColumnHeader();
             columnHeader3 = new ColumnHeader();
             columnHeader4 = new ColumnHeader();
+            groupBox1 = new GroupBox();
+            btnUpdate = new Button();
+            cbSecteur = new ComboBox();
+            label3 = new Label();
+            label1 = new Label();
+            tbLogin = new TextBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            cbSecteurAdd = new ComboBox();
+            label2 = new Label();
+            label4 = new Label();
+            tbLoginAdd = new TextBox();
+            cbRoleAdd = new ComboBox();
+            label5 = new Label();
+            tbMdpAdd = new TextBox();
+            label6 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // lvUser
@@ -40,10 +58,11 @@
             lvUser.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
             lvUser.Location = new Point(12, 12);
             lvUser.Name = "lvUser";
-            lvUser.Size = new Size(375, 394);
+            lvUser.Size = new Size(375, 426);
             lvUser.TabIndex = 0;
             lvUser.UseCompatibleStateImageBehavior = false;
             lvUser.View = View.Details;
+            lvUser.SelectedIndexChanged += lvUser_SelectedIndexChanged;
             // 
             // columnHeader1
             // 
@@ -63,15 +82,174 @@
             // 
             columnHeader4.Text = "Zone";
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnUpdate);
+            groupBox1.Controls.Add(cbSecteur);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(tbLogin);
+            groupBox1.Location = new Point(393, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(403, 159);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Modification d'un utilisateur";
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(97, 112);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(149, 41);
+            btnUpdate.TabIndex = 2;
+            btnUpdate.Text = "Sauvegarder les modifications";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // cbSecteur
+            // 
+            cbSecteur.FormattingEnabled = true;
+            cbSecteur.Location = new Point(97, 70);
+            cbSecteur.Name = "cbSecteur";
+            cbSecteur.Size = new Size(121, 23);
+            cbSecteur.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Secteur :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Identifiant :";
+            // 
+            // tbLogin
+            // 
+            tbLogin.Location = new Point(97, 34);
+            tbLogin.MaxLength = 50;
+            tbLogin.Name = "tbLogin";
+            tbLogin.Size = new Size(276, 23);
+            tbLogin.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(tbMdpAdd);
+            groupBox2.Controls.Add(cbRoleAdd);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(cbSecteurAdd);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(tbLoginAdd);
+            groupBox2.Location = new Point(393, 187);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(403, 251);
+            groupBox2.TabIndex = 8;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Ajout d'un utilisateur";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(97, 204);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 41);
+            button1.TabIndex = 2;
+            button1.Text = "Ajouter l'utilisateur";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // cbSecteurAdd
+            // 
+            cbSecteurAdd.FormattingEnabled = true;
+            cbSecteurAdd.Location = new Point(97, 153);
+            cbSecteurAdd.Name = "cbSecteurAdd";
+            cbSecteurAdd.Size = new Size(121, 23);
+            cbSecteurAdd.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Secteur :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 37);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Identifiant :";
+            // 
+            // tbLoginAdd
+            // 
+            tbLoginAdd.Location = new Point(97, 34);
+            tbLoginAdd.MaxLength = 50;
+            tbLoginAdd.Name = "tbLoginAdd";
+            tbLoginAdd.Size = new Size(276, 23);
+            tbLoginAdd.TabIndex = 2;
+            // 
+            // cbRoleAdd
+            // 
+            cbRoleAdd.FormattingEnabled = true;
+            cbRoleAdd.Location = new Point(97, 112);
+            cbRoleAdd.Name = "cbRoleAdd";
+            cbRoleAdd.Size = new Size(121, 23);
+            cbRoleAdd.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(55, 115);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Rôle :";
+            // 
+            // tbMdpAdd
+            // 
+            tbMdpAdd.Location = new Point(97, 74);
+            tbMdpAdd.MaxLength = 50;
+            tbMdpAdd.Name = "tbMdpAdd";
+            tbMdpAdd.Size = new Size(276, 23);
+            tbMdpAdd.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(8, 77);
+            label6.Name = "label6";
+            label6.Size = new Size(83, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Mot de passe :";
+            // 
             // Gestion_utilisateur
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(lvUser);
             Name = "Gestion_utilisateur";
             Text = "Gestion_utilisateur";
             Load += Gestion_utilisateur_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -81,5 +259,21 @@
         private ColumnHeader columnHeader3;
         private ColumnHeader columnHeader4;
         public ListView lvUser;
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox tbLogin;
+        private Button btnUpdate;
+        private ComboBox cbSecteur;
+        private Label label3;
+        private GroupBox groupBox2;
+        private Button button1;
+        private ComboBox cbSecteurAdd;
+        private Label label2;
+        private Label label4;
+        private TextBox tbLoginAdd;
+        private Label label6;
+        private TextBox tbMdpAdd;
+        private ComboBox cbRoleAdd;
+        private Label label5;
     }
 }
--- PREP-ORDER/PREP-ORDER/Gestion_utilisateur.cs
+++ PREP-ORDER/PREP-ORDER/Gestion_utilisateur.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using Microsoft.VisualBasic.Logging;
 using Microsoft.VisualBasic.ApplicationServices;
+using static PREP_ORDER.Gestion_utilisateur;
 
 namespace PREP_ORDER
 {
@@ -40,7 +41,7 @@ namespace PREP_ORDER
                                 int userID = Convert.ToInt32(reader["UserID"]);
                                 string userLogin = reader["login"].ToString();
                                 string role = reader["Role"].ToString();
-                                string zone = reader["codeZone"] != DBNull.Value ? reader["codeZone"].ToString() : "Aucune";
+                                string zone = reader["libelleZone"] != DBNull.Value ? reader["libelleZone"].ToString() : "Aucune";
 
                                 users.Add((userID, userLogin, role, zone));
                             }
@@ -49,11 +50,118 @@ namespace PREP_ORDER
                 }
                 return users;
             }
+
+            public static void ModifUser(int id, string login, string role, string zone)
+            {
+                using (SqlConnection connection = new SqlConnection(Program.GetConnectionString()))
+                {
+                    using (SqlCommand command = new SqlCommand("prc_update_user", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@id", id);
+                        command.Parameters.AddWithValue("@login", login);
+                        command.Parameters.AddWithValue("@role", role);
+                        command.Parameters.AddWithValue("@zone", zone);
+
+                        connection.Open();
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
         }
 
         private void Gestion_utilisateur_Load(object sender, EventArgs e)
         {
             // Vider la ListView
+            lvUser.Items.Clear();
+
+            // Récupérer les données
+            var users = User.GetAllUsers();
+
+            // Parcourir les résultats et ajouter chaque utilisateur
+            foreach (var user in users)
+            {
+                ListViewItem item = new ListViewItem(user.userID.ToString()); // ID
+                item.SubItems.Add(user.login);                               // Login
+                item.SubItems.Add(user.role);                                // Role
+                item.SubItems.Add(user.zone);                                // Zone
+
+                lvUser.Items.Add(item); // Ajouter à la ListView
+            }
+
+            //ajouter les secteurs à la comboBox dédiée
+            cbSecteur.Items.Add("Sec");
+            cbSecteur.Items.Add("Liquide");
+            cbSecteur.Items.Add("DPH");
+
+            tbLogin.Enabled = false;
+            cbSecteur.Enabled = false;
+        }
+
+        int id = 0;
+        string login = "";
+        string role = "";
+        string secteur = "";
+
+        private void lvUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvUser.SelectedItems.Count > 0)
+            {
+                // Obtenir l'élément sélectionné
+                ListViewItem selectedItem = lvUser.SelectedItems[0];
+
+                id = int.Parse(selectedItem.SubItems[0].Text);
+
+                login = selectedItem.SubItems[1].Text;
+
+                role = selectedItem.SubItems[2].Text;
+
+                secteur = selectedItem.SubItems[3].Text;
+
+                cbSecteur.Text = secteur;
+                tbLogin.Text = login;
+                tbLogin.Enabled = true;
+
+                if (secteur != "Aucune")
+                {
+                    cbSecteur.Enabled = true;
+                }
+            }
+            else
+            {
+                tbLogin.Enabled = false;
+                cbSecteur.Enabled = false;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbLogin.Text))
+            {
+                MessageBox.Show("Le champ Login ne peut pas être vide.");
+                return;
+            }
+
+            if (role == "RESPONSABLE" && secteur != "Aucune")
+            {
+                MessageBox.Show("Un responsable ne peut pas avoir de secteur assigné.");
+                return;
+            }
+
+            secteur = cbSecteur.Text; // Met à jour le secteur depuis la ComboBox
+
+            try
+            {
+                // Mise à jour de l'utilisateur
+                User.ModifUser(id, tbLogin.Text, role, secteur);
+                MessageBox.Show("Utilisateur mis à jour avec succès !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la mise à jour : {ex.Message}");
+            }
+
             lvUser.Items.Clear();
 
             // Récupérer les données
